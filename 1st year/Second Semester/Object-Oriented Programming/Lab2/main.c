35be59eb098a3f3f54f05ed34a138a4e9b3e90b3
iulian39 <hojdaiulian39@gmail.com>
2017-03-08 10:05:49 +0200

    .

--- 1st year/Second Semester/Object-Oriented Programming/Lab2/main.c
+++ 1st year/Second Semester/Object-Oriented Programming/Lab2/main.c
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -16,132 +17,144 @@ int* ReadArray();
 
 int main()
 {
-    char command;
-    while ( 1 )
-    {
-        Menu();
-        printf("Please enter the command : ");
-        scanf("\n%c", &command);
+	char command;
+	while (1)
+	{
+		Menu();
+		printf("Please enter the command : ");
+		scanf("\n%c", &command);
 
-        if ( command == '1' )
-            GeneratePrimeNumbers();
-        else
-            if ( command == '2' )
-            {
-                int *sir1 = malloc(sizeof(int) * 200);
-                sir1 = FindLongestSubsequence();
-                int i;
-                for (i = 0; i < 200; i++)
-                {
-                    if (sir1[i] == 0)
-                        break;
-                    printf("%d ", sir1[i]);
-                }
-                free(sir1);
-                printf("\n");
-            }
-            else
-                if ( command == '0' )
-                    break;
-    }
-    return 0;
+		if (command == '1')
+			GeneratePrimeNumbers();
+		else
+			if (command == '2')
+			{
+				int *sir1 = malloc(sizeof(int) * 200);
+				sir1 = FindLongestSubsequence();
+				int i;
+				for (i = 0; i < 200; i++)
+				{
+					if (sir1[i] == 0)
+						break;
+					printf("%d ", sir1[i]);
+				}
+				free(sir1);
+				printf("\n");
+			}
+			else
+				if (command == '0')
+					break;
+	}
+	return 0;
 }
 //The menu of the program
 void Menu()
 {
-    printf("Available Commands:\n");
-    printf("1 - Generate all the prime numbers smaller than a given natural number n\n");
-    printf("2 - Given a vector of numbers, find the longest increasing contiguous subsequence\n");
-    printf("0 - Exit\n");
+	printf("Available Commands:\n");
+	printf("1 - Generate all the prime numbers smaller than a given natural number n\n");
+	printf("2 - Given a vector of numbers, find the longest increasing contiguous subsequence\n");
+	printf("0 - Exit\n");
 }
 
 //The function used to generate all the prime numbers below a given number
 void GeneratePrimeNumbers()
 {
-    int n, i;
-    printf("\nPlease enter the number :");
-    scanf("%d", &n);
-    for (i = 2; i < n; ++i)
-        if(IsPrime(i) == true)
-            printf("%d ", i);
-    printf("\n");
+	int n, i;
+	printf("\nPlease enter the number :");
+	scanf("%d", &n);
+	for (i = 2; i < n; ++i)
+		if (IsPrime(i) == true)
+			printf("%d ", i);
+	printf("\n");
 
 }
 
 //Checks if a number is prime or not
 bool IsPrime(int nr)
 {
-    if ( nr < 2 )
-        return false;
-    if ( nr == 2)
-        return true;
-    int i;
-    for (i = 2; i <= nr/2 ; i++)
-        if(nr % i == 0)
-            return false;
-    return true;
+	if (nr < 2)
+		return false;
+	if (nr == 2)
+		return true;
+	int i;
+	for (i = 2; i <= nr / 2; i++)
+		if (nr % i == 0)
+			return false;
+	return true;
 }
 
 //Reads the array
 int* ReadArray()
 {
-    int x, i = 0;
-    int *sir1 = malloc(sizeof(int) * 200);
-    while (1)
-    {
-        scanf("%d", &x);
-        if (x == 0)
-            break;
-        else
-            sir1[i++] = x;
-    }
+	int x, i = 0;
+	int *sir1 = malloc(sizeof(int) * 200);
+	while (1)
+	{
+		scanf("%d", &x);
+		if (x == 0)
+			break;
+		else
+			sir1[i++] = x;
+	}
 
-    sir1[i] = 0;
-    return sir1;
+	sir1[i] = 0;
+	return sir1;
 }
 
 //Given a vector of numbers, find the longest increasing contiguous subsequence
 int* FindLongestSubsequence()
 {
-    int x, i = 0, maxLen = -1, j, poz1 = 0, poz2 = 0, cnt = 0;
-    int *sir1 = ReadArray();
-    int len_sir = 0;
-    for (i = 0; i < 200; i++) {
-        if (sir1[i] == 0) 
-            break;
-        len_sir = i + 1;
-    }
-    for (i = 0; i < len_sir - 1; ++i)
-        if (sir1[i] < sir1[i + 1])
-        {
-            cnt = 2;
-            for ( j = i + 1; j <= len_sir - 1; ++j )
-                if(sir1[j] < sir1[j + 1])
-                    cnt++;
-                else
-                {
-                    if (cnt > maxLen)
-                    {
-                        maxLen = cnt;
-                        poz1 = i;
-                        poz2 = j;
-                    }
-                    i += j - 1;
-                    break;
-                }
+	int x, i = 0, maxLen = -1, j, poz1 = 0, poz2 = 0, cnt = 0;
+	int *sir1 = ReadArray();
+	int len_sir = 0;
+	for (i = 0; i < 200; i++) {
+		if (sir1[i] == 0)
+			break;
+		len_sir = i + 1;
+	}
+	for (i = 0; i < len_sir - 1; ++i)
+		if (sir1[i] < sir1[i + 1])
+		{
+			cnt = 2;
+			for (j = i + 1; j < len_sir - 1; ++j)
+				if (sir1[j] < sir1[j + 1])
+				{
+					cnt++;
+					if (j + 1 == len_sir - 1)
+					{
+						cnt++;
+						if (cnt > maxLen)
+						{
+							maxLen = cnt;
+							poz1 = i;
+							poz2 = j + 1;
+						}
 
-        }
-    if ( poz2 == 0)
-        poz2 = len_sir - 1;
+					}
+				}					
+				else
+				{
+					if (cnt > maxLen)
+					{
+						maxLen = cnt;
+						poz1 = i;
+						poz2 = j;
+					}
+					i = j - 1;
+					break;
+				}
 
-    j = -1;
-    int *sir2 = malloc(sizeof(int) * 200);
-    for ( i = poz1; i <= poz2; ++i)
-        sir2[++j] = sir1[i];
-    sir2[++j] = 0;
-    free(sir1);
+		}
+	if (poz2 == 0)
+		poz2 = len_sir - 1;
 
-    return sir2;
+	j = -1;
+	int *sir2 = malloc(sizeof(int) * 200);
+	for (i = poz1; i <= poz2; ++i)
+		sir2[++j] = sir1[i];
+	sir2[++j] = 0;
+	free(sir1);
+
+	return sir2;
 
 }
-
